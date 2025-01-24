7d8b703eecfc3559d89a3aea30f98d9f47650d9c
qaltx <67969197+notqaltx@users.noreply.github.com>
2025-01-25 00:27:54 +0200

    - added shell command into cli (must fully re-wrote cli and main file)

--- src/cli.rs
+++ src/cli.rs
@@ -11,6 +11,10 @@ fn main() {
                 .arg(Arg::new("file").help("The .nua file to run").required(true).index(1)),
         )
         .subcommand(
+            Command::new("shell")
+                .about("Runs a nua shell for testing purposes")
+        )
+        .subcommand(
             Command::new("test")
                 .about("Runs a Lua file in the ./src/tests folder")
                 .arg(Arg::new("file").help("The Lua file to run in ./src/tests/lua").required(true).index(1)),
@@ -29,15 +33,30 @@ fn main() {
                 eprintln!("{}", e);
             }
         }
+    } else if matches.subcommand_matches("shell").is_some() {
+        if let Err(e) = run_nua_shell() {
+            eprintln!("{}", e); 
+        }
     }
 }
-    
 fn run_nua_file(file: &str) -> Result<()> {
     let lua = Lua::new();
     let code = fs::read_to_string("./src/lang/main.lua").expect("Could not read file");
 
     lua.context(|ctx| {
         ctx.globals().set("arg", vec![file])?;
+        match ctx.load(&code).exec() {
+            Ok(_) => (),
+            Err(e) => println!("{}", e),
+        }
+        Ok(())
+    })
+}
+fn run_nua_shell() -> Result<()> {
+    let lua = Lua::new();
+    let code = fs::read_to_string("./src/lang/main.lua").expect("Could not read file");
+
+    lua.context(|ctx| {
         match ctx.load(&code).exec() {
             Ok(_) => (),
             Err(e) => println!("{}", e),
